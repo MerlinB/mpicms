3559fd818e3dfbbe01744f5a29a97a6f0e39f330
Merlin Buczek <merlin.buczek@pm.me>
2019-05-12 12:28:45 +0200

    Set up LDAP, split settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,8 +1,5 @@
-import ldap
-from django_auth_ldap.config import LDAPSearch, GroupOfNamesType
-
-from .base import *  # noqa
-from .base import env
+from .ldap import *  # noqa
+from .ldap import env
 
 # GENERAL
 # ------------------------------------------------------------------------------
@@ -16,38 +13,6 @@ ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['twiki.molgen.mpg.de']
 DATABASES['default'] = env.db('DATABASE_URL')  # noqa F405
 DATABASES['default']['ATOMIC_REQUESTS'] = True  # noqa F405
 DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=60)  # noqa F405
-
-# AUTHENTICATION
-AUTHENTICATION_BACKENDS = [
-    'django_auth_ldap.backend.LDAPBackend',
-    # 'django.contrib.auth.backends.ModelBackend',
-]
-
-AUTH_LDAP_SERVER_URI = env('LDAP_URI')
-
-AUTH_LDAP_BIND_DN = ""
-AUTH_LDAP_BIND_PASSWORD = ""
-AUTH_LDAP_USER_SEARCH = LDAPSearch(
-    "ou=users,dc=example,dc=com", ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
-
-AUTH_LDAP_FIND_GROUP_PERMS = True
-AUTH_LDAP_GROUP_SEARCH = LDAPSearch(
-    'ou=groups,dc=example,dc=com',
-    ldap.SCOPE_SUBTREE,
-    '(objectClass=groupOfNames)',
-)
-AUTH_LDAP_GROUP_TYPE = GroupOfNamesType(name_attr='cn')
-
-AUTH_LDAP_USER_ATTR_MAP = {
-    'first_name': 'givenName',
-    'last_name': 'sn',
-    'email': 'mail',
-}
-
-AUTH_LDAP_USER_FLAGS_BY_GROUP = {
-    'is_staff': 'cn=staff,ou=groups,dc=example,dc=com',
-    'is_superuser': 'cn=superuser,ou=groups,dc=example,dc=com',
-}
 
 # SECURITY
 # ------------------------------------------------------------------------------
