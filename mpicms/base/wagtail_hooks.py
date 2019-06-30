4081d751294bc3f9753f666ad47a8d5f6cd7f93f
Merlin Buczek <merlin.buczek@pm.me>
2019-06-30 21:42:59 +0200

    Add return to site button

--- mpicms/base/wagtail_hooks.py
+++ mpicms/base/wagtail_hooks.py
@@ -2,6 +2,8 @@ from django.utils.translation import gettext as _
 
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 from wagtail.core import hooks
+from wagtail.admin.menu import MenuItem
+
 # from django.contrib.auth.models import Permission
 
 from .models import Banner
@@ -22,6 +24,11 @@ def hide_snippets_menu_item(request, menu_items):
 def register_blockquote_feature(features):
     """h1 headings should be allowed in rich text, as they can be used in markdown anyway."""
     features.default_features.insert(0, 'h1')
+
+
+@hooks.register('register_admin_menu_item')
+def register_site_button():
+    return MenuItem(_('View website'), '/', classnames='icon icon-site', order=1)
 
 
 class BannerAdmin(ModelAdmin):
