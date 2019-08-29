d44df5ee7f8a4de58401df256efa2969a35ed1cd
Merlin Buczek <merlin.buczek@pm.me>
2019-08-29 10:07:04 +0200

    Fix explorer navigation

--- mpicms/news/models.py
+++ mpicms/news/models.py
@@ -20,7 +20,7 @@ class NewsPage(CategoryMixin, BasePage):
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
 
-        if self.is_root:
+        if self.is_news_root:
             context['blogs'] = NewsPage.objects.all()
 
         paginator = Paginator(self.news_items, self.paginated_by)
@@ -38,12 +38,12 @@ class NewsPage(CategoryMixin, BasePage):
         return context
 
     @property
-    def is_root(self):
+    def is_news_root(self):
         return self.depth <= 3
 
     @property
     def news_items(self):
-        if self.is_root:
+        if self.is_news_root:
             return NewsEntry.objects.live().order_by('-date')
         return NewsEntry.objects.child_of(self).live().order_by('-date')
 
