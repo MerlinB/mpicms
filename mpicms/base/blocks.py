c4e89b3d706da07b75c900eb80dcbdc8f15826b9
Merlin Buczek <merlin.buczek@pm.me>
2019-09-03 17:00:18 +0200

    Fix table overflow

--- mpicms/base/blocks.py
+++ mpicms/base/blocks.py
@@ -1,12 +1,16 @@
 from django.utils.translation import gettext_lazy as _
 
 from wagtail.core import blocks
-from wagtail.contrib.table_block.blocks import TableBlock
+from wagtail.contrib.table_block.blocks import TableBlock as WagtailTableBlock
 
 
 class MarkdownBlock(blocks.TextBlock):
     class Meta:  # noqa
         template = 'base/blocks/markdown_block.html'
+
+class TableBlock(WagtailTableBlock):
+    class Meta:  # noqa
+        template = 'base/blocks/table_block.html'
 
 
 class ContentBlock(blocks.StreamBlock):
