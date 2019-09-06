5ff3d3738c328a7cd5942c4c3d3f8eed59c9dd96
Merlin Buczek <merlin.buczek@pm.me>
2019-09-06 10:37:45 +0200

    Improve room link detection

--- mpicms/base/utils.py
+++ mpicms/base/utils.py
@@ -5,4 +5,5 @@ def can_create(request, model):
     return not getattr(model, 'creation_limited', False)
 
 def get_room_link(room):
-    return 'https://twiki.molgen.mpg.de/foswiki/bin/room/' + room
+    if room:
+        return 'https://twiki.molgen.mpg.de/foswiki/bin/room/' + room.split()[0]
